ae59bfdda2ca6e89e3b19ad1965acb64c246addc
Amandine <amandine.labaeye@orange.fr>
2018-11-23 10:32:28 +0100

    commit

--- exercice1.js
+++ exercice1.js
@@ -1,8 +1,32 @@
 var catEl = document.getElementById("cat");
 
 var startTime = new Date().getTime();
+
 var walkTheCat = function() {
+
     var currTime = new Date().getTime();
+
     var secondsElapsed = ((currTime - startTime)/1000);
+
+    var left = secondsElapsed * 100;
+
+    catEl.style.left = left + "px";
+
+    catEl.style.top = "200px";
+
+    if (left < 1180) {
+
+    requestAnimationFrame(walkTheCat);
+
+    } else {
+
+        alert("Ouuuuuutch le muuuur !")
+        location.reload();
+    }
+
+
+
 };
-walkTheCat();
+
+requestAnimationFrame(walkTheCat);
+
